2f5e1726a3787c455aa85c0addbdd01829a6e242
1JJ1 <69682351+1JJ1@users.noreply.github.com>
2021-04-16 20:42:53 -0500

    Add files via upload

---
+++ scripts/dataCourses.js
@@ -0,0 +1,11 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW", "Cesar Forero", 3),
+    new Course("Arquitectura Empresarial", "Maria Camila Romero", 3),
+    new Course("Lenguajes y Máquinas", "Nicolas Cardozo", 2),
+    new Course("Sistemas Transaccionales", "Anamaría Irmgard Mojica", 3),
+    new Course("Diseño y Análisis de Algoritmos", "Jorge Duitama", 4),
+    new Course("Sistemas de Manufactura", "Fabio Arturo Rojas", 4),
+    new Course("Sistemas de Conversión de Energía", "Álvaro Enrique Pinila", 3),
+    new Course("Aleman 2", "Claudia Rocio Vargas", 2)
+];
---
+++ scripts/dataStudent.js
@@ -0,0 +1,8 @@
+import { Student } from './student.js';
+export var dataStudent = [
+    new Student("Código", "201812319"),
+    new Student("Cédula", "1010453789"),
+    new Student("Edad", "20 Años"),
+    new Student("Dirección", "Carrera falsa 37 No. 27-55"),
+    new Student("Telefono", "3112789655")
+];
